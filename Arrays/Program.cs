e70c05d43b4efa7546e6934f3d6ca9ba47e0d1d1
ArtemEvgTitov <>
2022-08-14 14:33:36 +0300

    Задача на нахождение ближайшего элемента решена

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -12,57 +12,58 @@
 Console.Write("\nВведите количество элементов массива: ");
 int size = Convert.ToInt32(Console.ReadLine());
 int[] array = new int[size];
+
 for (int i = 0; i < size; i++)
 {
     array[i] = new Random().Next(1, 100);
 }
 Console.WriteLine("Начальный массив: [" + string.Join(", ", array) + "]");
+
 Console.Write("Введите число: ");
 int X = Convert.ToInt32(Console.ReadLine());
-int CloseInSize(int[] array, int X)
-{
-    int[] tempArray = new int[array.Length + 1];
-    tempArray[tempArray.Length - 1] = X;
-    for (int i = 0; i < tempArray.Length - 1; i++)
-        tempArray[i] = array[i];
-    for (int i = 0; i < tempArray.Length; i++)
-    {
-        for (int j = 0; j < tempArray.Length - 1; j++)
-        {
-            if (tempArray[j] > tempArray[j + 1])
-            {
-                int temp = tempArray[j];
-                tempArray[j] = tempArray[j + 1];
-                tempArray[j + 1] = temp;
-            }
-        }
-    }
-    int indexX = 0;
-    for (int i = 0; i < tempArray.Length; i++)
-    {
-        if (tempArray[i] == X)
-        {
-            indexX = i;
-            break;
-        }
-    }
-    int closeInSize = 0;
-    if (indexX == tempArray.Length - 1)
-        closeInSize = tempArray[indexX - 1];
-    if (indexX == 0)
-        closeInSize = tempArray[indexX + 1];
-    int closeInSize2 = 0;
-    if (indexX != tempArray.Length - 1 && indexX != 0)
-    {
-        closeInSize = tempArray[indexX - 1];
-        closeInSize2 = tempArray[indexX + 1];
-    }
-    if (closeInSize > closeInSize2) closeInSize = closeInSize2;
-    if (closeInSize < closeInSize2) return closeInSize;
-    if (closeInSize == closeInSize2) return closeInSize;
 
-    return closeInSize;
+int[] tempArray = new int[size + 1];
+tempArray[tempArray.Length - 1] = X;
+for (int i = 0; i < tempArray.Length - 1; i++)
+    tempArray[i] = array[i];
+
+for (int i = 0; i < tempArray.Length; i++)
+{
+    for (int j = 0; j < tempArray.Length - 1; j++)
+    {
+        if (tempArray[j] > tempArray[j + 1])
+        {
+            int temp = tempArray[j];
+            tempArray[j] = tempArray[j + 1];
+            tempArray[j + 1] = temp;
+        }
+    }
+}
+Console.WriteLine("Временный массив: [" + string.Join(", ", tempArray) + "]");
+int indexX = 0;
+for (int i = 0; i < tempArray.Length; i++)
+{
+    if (tempArray[i] == X)
+    {
+        indexX = i;
+        break;
+    }
 }
 
-int closeInSize = CloseInSize(array, X);
-Console.WriteLine($"\nБлижайшее число равно: {closeInSize}");
+int closeInSize = 0;
+int closeInSize2 = 0;
+
+if (indexX == tempArray.Length - 1)
+    closeInSize = tempArray[indexX - 1];
+if (indexX == 0)
+    closeInSize = tempArray[indexX + 1];
+    closeInSize2 = tempArray[indexX + 2];
+if (indexX != tempArray.Length - 1 && indexX != 0)
+{
+    closeInSize = tempArray[indexX - 1];
+    closeInSize2 = tempArray[indexX + 1];
+}
+
+if (closeInSize > closeInSize2) Console.WriteLine($"Ближайшее число равно: {closeInSize2}");
+if (closeInSize < closeInSize2) Console.WriteLine($"Ближайшее число равно: {closeInSize}");
+if (closeInSize == closeInSize2) Console.WriteLine($"Ближайшее число равно: {closeInSize}");
